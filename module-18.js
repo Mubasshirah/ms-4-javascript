f66b6f050d350b9099e6b88c2659657be36ddb77
Mubasshirah <adibaju1994@gmail.com>
2023-02-01 10:38:22 +0600

    k

--- module-18.js
+++ module-18.js
@@ -1,2 +1,86 @@
-var name=["adiba","adi","diba","adib"];
-console.log(name.length);
+var names=["adiba","adi","diba","adib"];
+// console.log(names.length);
+                // console.log(names[1]);
+       // names[0]="tanjib";
+       // console.log(names)
+                        // console.log(names.indexOf("adib"));
+  // names.push("sadat")
+  // console.log(names);
+  // names.pop()
+//   console.log(names);
+//               var element=names.pop();
+//               console.log(names);
+//               console.log(element);
+// names.unshift("radia") 
+// console.log(names)
+       // names.shift()
+      //console.log(names)
+                                // var chickenPrice=500;
+                                // var myBudget=5000;
+                                // if(chickenPrice>=myBudget){
+                                //   console.log('alu vorta enough');
+                                // }
+                                // else{
+                                //     console.log('murgi khbo');
+                                // }
+            // var isGraduate=true;
+            // var car=1;
+           // var salary=30000;
+          // if(isGraduate==false  || salary==40000 && car>=1){
+          //   console.log('eligible');
+         // }
+           // else{
+     //   console.log('not eligible');
+        // }                            
+
+                                      // var money=51;
+                                      // var toast=30;
+                                      // var butterToast=40;
+                                      // var gheeToast=50;
+                                      // if(money>toast && money<butterToast){
+                                      //   console.log('toast');
+                                      // }
+                                      // else if(money>butterToast && money<gheeToast){
+                                      //   console.log('butter toast');
+                                      // }
+                                      // else if(money>gheeToast){
+                                      //   console.log('ghee toast');
+                                      // }
+                                      // else{
+                                      //   console.log('kisui khabona');
+                                      // }
+
+
+                          // var num1=9;
+                          // var num2=9;
+                          // var num3=9;
+                          // if(num1>num2 && num1>num3){
+                          //   console.log('13 is the largest')
+                          // }
+                          // else if(num2>num1 && num2>num3){
+                          //   console.log('79 is the largest')
+                          // }
+                          // else if(num3>num2 && num3>num1){
+                          //   console.log('45 is the largest')
+                          // }
+                          // else{
+                          //   console.log('all are equal')
+                          // } 
+
+
+                                          // var side1=9;
+                                          // var side2=9;
+                                          // var side3=9;
+                                          // if(side1==side2 && side1!=side3 ){
+                                          //   console.log('it is a isoscale 1')
+                                          // }
+                                          // else if(side2==side3 && side2!=side1 ){
+                                          //   console.log('it is a isoscale 2')
+                                          // }
+                                          // else if(side3==side1 && side3!=side2 ){
+                                          //   console.log('it is a isoscale 3')
+                                          // }
+                                          // else {
+                                          //   console.log('it is not')
+                                          //   }
+                                          
