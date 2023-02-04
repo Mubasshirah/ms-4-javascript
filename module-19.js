80bb6aa313ce07b1b9c247599da4aa9f781378f7
Mubasshirah <adibaju1994@gmail.com>
2023-02-04 21:19:12 +0600

    j

---
+++ module-19.js
@@ -0,0 +1,72 @@
+// var roastGiven=0;
+// while(roastGiven<7){
+//     console.log('roast given');
+//     console.log(roastGiven);
+//     roastGiven++;
+// }
+                    // var num=2;
+                    // while(num<=100){
+                    //     console.log(num);
+                    //     num=num+2;
+                    // }
+                    // var num=1;
+                    // while(num<=100){
+                    //     console.log(num);
+                    //     num=num+2;
+                    // }
+
+        // for(var num=1;num<=10;num=num+2){
+        //     console.log(num);
+        // }
+
+                        // var items=['book','pen','eraser','sharpnar','scale'];
+                        // for(var i=0;i<items.length;i++){
+                        //     var item=items[i];
+                        //     console.log(item);
+                        // }
+
+                    //   var items=['book','pen','eraser','sharpnar','scale'];
+                    //     for(var i=0;i<items.length;i++){
+                    //         var item=items[i];
+                    //         console.log(item);
+                    //         if(item=='pen'){
+                    //             break;
+                    //         }
+                    //     }
+
+    // var num=0;
+    // for(var i=0;i<10;i++){
+    //     
+    //     if(i>5){
+    //         break;
+    //     }
+    // console.log(i);
+    // }
+
+            // var numbers=[23,43,50,54,65,55,27,29,44];
+            // for(var i=0;i<numbers.length;i++){
+            //          var number=numbers[i];
+                    
+            //          if(number>50){
+            //             continue;
+            //          }
+            //          console.log(number);
+
+            // }
+
+                                //  var num=10;
+                                //  while(num>=1){
+                                //     console.log(num);
+                                //     num--
+                                //  }
+
+                        // var num=20;
+                        // for(var i=20;i>=0;i--){
+                        //     console.log(i);
+                        // }
+
+// var i = 0;
+for (var i=0; i<5; i++){
+    console.log(i);  
+};
+
